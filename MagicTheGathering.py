b17e75652da30e3604fcca8fa0aa80ce32018df4
Marcelo Mariduena <marcelomariduena10@gmail.com>
2020-03-27 16:25:09 -0400

    all csv files were read and filtered under MagicTheGathering.py

--- MagicTheGathering.py
+++ MagicTheGathering.py
@@ -2,6 +2,19 @@ import numpy
 import pandas 
 
 # read the csv file 
-dataframe = pandas.read_csv("./secondEdition.csv")
-dataset = dataframe.values
-print(dataset)
+dataframe1 = pandas.read_csv("./secondEdition.csv")
+dataframe2 = pandas.read_csv("./thirdEdition.csv")
+dataframe3 = pandas.read_csv("./fourthEdition.csv")
+
+# convert it into a neat table 
+dataframe1 = pandas.DataFrame(dataframe1)
+dataframe2 = pandas.DataFrame(dataframe2)
+dataframe3 = pandas.DataFrame(dataframe3)
+
+# remove rows with missing values
+dataframe1.dropna()
+dataframe2.dropna()
+dataframe3.dropna()
+
+# testing
+print(dataframe1)
