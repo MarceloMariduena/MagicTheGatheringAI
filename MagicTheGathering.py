831ec67c7280005e764e6300a11666e41b1c7b4d
Marcelo Mariduena <marcelomariduena10@gmail.com>
2020-04-06 16:04:53 -0400

    Goals were divided into steps under MagicTheGathering.py

--- MagicTheGathering.py
+++ MagicTheGathering.py
@@ -16,3 +16,27 @@ dataframe3 = pandas.DataFrame(dataframe3)
 
 features = dataframe1[['type', 'subtypes', 'rarity']].dropna()
 print(features)
+
+
+# TODO: Code
+# 1. Get a dataset
+# 2. Encode features
+# 3. Take user input (Name, Description, Mana cost)
+# 4. Generate the following:
+#       4.1. Effects
+#       4.2. Type
+#       4.3. Health
+#       4.4. Power
+
+
+# TODO: App
+# 1. Allow user to input photo, name, description, mana cost.
+# 2. Return a MagicTheGathering card with:
+#       2.1. *Animated (MagicTheGathering) photo of the user
+#       2.1. Name
+#       2.1. Description
+#       2.1. Mana cost
+#       2.1. *Effects
+#       2.1. *Type
+#       2.1. *Health
+#       2.1. *Power
