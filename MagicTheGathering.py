8fde657494562e014e2e5b49115840901250079e
Marcelo Mariduena <marcelomariduena10@gmail.com>
2020-04-07 01:10:54 -0400

    Data was split into groups. A classifier will need to be chosen.

--- MagicTheGathering.py
+++ MagicTheGathering.py
@@ -1,44 +1,55 @@
-#! resources: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.dropna.html
-
-import numpy 
-import pandas 
-
-# read the csv file 
-dataframe1 = pandas.read_csv("./Datasets/secondEdition.csv")
-dataframe2 = pandas.read_csv("./Datasets/thirdEdition.csv")
-dataframe3 = pandas.read_csv("./Datasets/fourthEdition.csv")
-
-# convert it into a neat table 
-dataframe1 = pandas.DataFrame(dataframe1)
-dataframe2 = pandas.DataFrame(dataframe2)
-dataframe3 = pandas.DataFrame(dataframe3)
-
-
-features = dataframe1[['type', 'subtypes', 'rarity']].dropna()
-print(features)
-
-
-# TODO: Code
-# 1. Get a dataset
-# 2. Process the data (get rid of incomplete rows of data)
-# 3. Encode features
-# 4. Make a classification or prediction model 
-# 5. Take user input (Name, Description, Mana cost)
-# 6. Generate the following for this user:
-#       6.1. Effects
-#       6.2. Type
-#       6.3. Health
-#       6.4. Power
-
+#! resources: https://pd.pydata.org/pd-docs/stable/reference/api/pd.DataFrame.dropna.html
 
 # TODO: App
-# 1. Allow user to input photo, name, description, mana cost.
+# 1. Allow user to input photo, [ name, text ].
 # 2. Return a MagicTheGathering card with:
 #       2.1. *Animated (MagicTheGathering) photo of the user
-#       2.1. Name
-#       2.1. Description
-#       2.1. Mana cost
-#       2.1. *Effects
-#       2.1. *Type
-#       2.1. *Health
-#       2.1. *Power
+#       2.1. name
+#       2.1. text
+#       2.1. type
+#       2.1. power
+#       2.1. toughness
+#       2.1. manaCost
+#       2.1. colorIdentity
+
+
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+
+
+# Read from csv files
+dataframe1 = pd.read_csv("./Datasets/secondEdition.csv")
+dataframe2 = pd.read_csv("./Datasets/thirdEdition.csv")
+dataframe3 = pd.read_csv("./Datasets/fourthEdition.csv")
+
+
+# Convert raw data into a neat tables
+dataframe1 = pd.DataFrame(dataframe1)
+dataframe2 = pd.DataFrame(dataframe2)
+dataframe3 = pd.DataFrame(dataframe3)
+
+
+# PROVIDED LABELS FROM DATASET: 
+#  [name, manaCost, cmc, colorIdentity, artist, number, type, text, printings, flavor, layout, multiverseid, power, toughness, rarity, subtypes, types]
+
+
+# LABELS TO WORK WITH: !userWillInput *modelOutput
+#   !name, !text, *type, *power, *toughness, *manaCost, *colorIdentity
+
+
+# SELECTING FEATURES 
+features = dataframe1[['name', 'text', 'type', 'power', 'toughness', 'manaCost', 'colorIdentity']].dropna()
+
+
+# SPLIT DATA INTO SETS:
+#   X: inputs
+#   y: outputs
+X = dataframe1[['name', 'text']].dropna() 
+y = dataframe1[['type', 'power', 'toughness', 'manaCost', 'colorIdentity']].dropna() 
+# print(X)
+# print(y)
+X = np.array(X)
+y = np.array(y)
+
