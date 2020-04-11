3ebce552cd383b223db712dbd4f40a32fd1a8d98
Marcelo Mariduena <marcelomariduena10@gmail.com>
2020-04-11 03:20:01 -0400

    almost done

--- MagicTheGathering.py
+++ MagicTheGathering.py
@@ -1,31 +1,113 @@
-import numpy as np
+# Read from csv files convert contents into tables.
 import pandas as pd
-from sklearn import preprocessing
-from sklearn.model_selection import train_test_split
-
-
-# Read from csv files
 dataframe1 = pd.read_csv("./Datasets/secondEdition.csv")
 dataframe2 = pd.read_csv("./Datasets/thirdEdition.csv")
 dataframe3 = pd.read_csv("./Datasets/fourthEdition.csv")
-
-
-# Convert raw data into a neat tables
 dataframe1 = pd.DataFrame(dataframe1)
 dataframe2 = pd.DataFrame(dataframe2)
 dataframe3 = pd.DataFrame(dataframe3)
 
 
-# PROVIDED LABELS FROM DATASET: 
-    #  [name, manaCost, cmc, colorIdentity, artist, number, type, text, printings, flavor, layout, multiverseid, power, toughness, rarity, subtypes, types]
-# WE WILL SELECT THE FOLLOWING: 
-    # LABEL: EXAMPLE
-    # name: 'Benalish Hero'
-    # text: "Banding (Any creatures with banding, and up to one without, can attack in a band."
-    # type: 'Creature — Human Soldier' 
-    # power: '1' 
-    # toughness:'1' 
-    # manaCost: '{W}' 
-    # colorIdentity: 'W'
-X = dataframe1[['name', 'text', 'type', 'power', 'toughness', 'manaCost', 'colorIdentity']].dropna()#.to_numpy()
-print(X)
+# Select the categories we will deal with. Ensure rows with missing data are not used.
+X = dataframe1[['text', 'type', 'power', 'toughness', 'manaCost', 'colorIdentity']].dropna().to_numpy()
+card_text = list(X[:,0])
+card_type = list(X[:,1])
+card_power = list(X[:,2])
+card_toughness = list(X[:,3])
+card_mana_cost = list(X[:,4])
+card_color_identity = list(X[:,5])
+
+
+# Train the model, CountVectorizer:
+    # Given that descriptions are series of ordered words, we can convert those series into numerical feature vectors
+from sklearn.feature_extraction.text import CountVectorizer
+count_vect = CountVectorizer()
+X_train_counts = count_vect.fit_transform(card_text)
+X_train_counts.shape
+
+
+# Reduce the weightage of certain words like (the, is, a, an, etc) 
+    # For this we will use Term Frequency times inverse document frequency (TfidTransformer) to invert the stats and return keywords. 
+from sklearn.feature_extraction.text import TfidfTransformer
+tfidf_transformer = TfidfTransformer()
+X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+X_train_tfidf.shape
+
+
+# # Encode the targets
+# from sklearn import preprocessing
+# card_type_encoder = preprocessing.LabelEncoder()
+# card_power_encoder = preprocessing.LabelEncoder()
+# card_toughness_encoder = preprocessing.LabelEncoder()
+# card_mana_cost_encoder = preprocessing.LabelEncoder()
+# card_color_identity_encoder = preprocessing.LabelEncoder()
+
+
+# Now we can begin the classification with various targets
+from sklearn.naive_bayes import MultinomialNB
+clf1 = MultinomialNB().fit(X_train_tfidf, card_type)
+clf2 = MultinomialNB().fit(X_train_tfidf, card_power)
+clf3 = MultinomialNB().fit(X_train_tfidf, card_toughness)
+clf4 = MultinomialNB().fit(X_train_tfidf, card_mana_cost)
+clf5 = MultinomialNB().fit(X_train_tfidf, card_color_identity)
+
+
+# Test data 
+X2 = dataframe2[['text', 'type', 'power', 'toughness', 'manaCost', 'colorIdentity']].dropna().to_numpy()
+card_text2 = list(X2[:,0])
+card_type2 = list(X2[:,1])
+card_power2 = list(X[:,2])
+card_toughness2 = list(X[:,3])
+card_mana_cost2 = list(X[:,4])
+card_color_identity2 = list(X[:,5])
+
+
+# Training another classifier
+from sklearn.pipeline import Pipeline
+text_clf = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), ('clf', MultinomialNB()),])
+text_clf = text_clf.fit(card_text, card_type)
+
+
+# Accuracy testing
+import numpy as np
+predicted = text_clf.predict(card_text2)
+print(np.mean(predicted == card_type2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # # PROVIDED LABELS FROM DATASET: 
+# #     #  [name, manaCost, cmc, colorIdentity, artist, number, type, text, printings, flavor, layout, multiverseid, power, toughness, rarity, subtypes, types]
+# # # WE WILL SELECT THE FOLLOWING: 
+# #     # LABEL: EXAMPLE
+# #     # name: 'Benalish Hero'
+# #     # text: "Banding (Any creatures with banding, and up to one without, can attack in a band."
+# #     # type: 'Creature — Human Soldier' 
+# #     # power: '1' 
+# #     # toughness:'1
+# #     # manaCost: '{W}' 
+# #     # colorIdentity: 'W'
+# # # categories = dataframe1[['name', 'text', 'type', 'power', 'toughness', 'manaCost', 'colorIdentity']].dropna().to_numpy()
+# # # X = dataframe1[['text']].dropna().to_numpy()
+# # # Z = dataframe1[['text']].dropna()
+# # # print(X)
+# # # print(Z)
+
+
+# # # Given that descriptions are series of ordered words, we can convert those series into numerical feature vectors
+# # # count_vect = CountVectorizer()
+# # # X_train_counts = count_vect.fit_transform(X[0])
